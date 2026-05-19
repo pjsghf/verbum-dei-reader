5f3dcd4a544036792fb86d58f88282eaa13f817e
pjsghf <pjsghf@gmail.com>
2026-05-19 11:45:22 -0300

    security: clean git repository, add ignores and remove internal script exposure

--- scratch/view_bosquejo_1.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('d:/Anti Gravity/projects/verbum-dei-reader/extracted_josue.txt', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-pages = re.split(r'=== PAGE \d+ ===', content)
-print(pages[6])
